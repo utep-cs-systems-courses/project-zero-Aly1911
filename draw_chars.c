ef35334f020163dad4e1df5fe4036d0fa0461a87
Aly1911 <agardea12@miners.utep.edu>
2021-09-19 16:49:39 -0600

    Fixed Font

--- draw_chars.c
+++ draw_chars.c
@@ -17,10 +17,11 @@ void print_char_11x16(char c)
 void print_char_8x12(char c)
 {
   c -= 0x20;
-  for (char row = 0; row < 8; row++) {
+  for (char col = 0; col < 8; col++) {
+    for (char row = 12; row > 0; row--) {
     unsigned short rowBits = font_8x12[c][row];
-    for (char col = 0; col < 12; col++) {
-      unsigned short colMask = 1 << (11-col); /* mask to select bit associated with bit */
+    /* mask to select bit associated with bit */
+      unsigned short colMask = 1<< (7-col);
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
